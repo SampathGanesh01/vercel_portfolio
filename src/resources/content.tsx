fcdc0769d96900afb40b596fee1bdba631cb834f
KANDREGULA SAMPATH GANESH <sampathganesh@ASBLL00159.local>
2026-06-28 19:57:55 +0530

    added link for booking calls

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -83,7 +83,7 @@ const about: About = {
   },
   calendar: {
     display: true,
-    link: "https://cal.com",
+    link: "https://calendar.app.google/dhtKugqZRteCY7VR9",
   },
   intro: {
     display: true,
